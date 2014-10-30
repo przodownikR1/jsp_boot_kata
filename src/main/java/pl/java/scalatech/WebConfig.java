6e5cb4d20bd1819aca6c3231405419dc90ea9799
Slawomir Borowiec <przodownik@tlen.pl>
2014-10-30 20:59:39 +0100

    clean code

---
+++ src/main/java/pl/java/scalatech/WebConfig.java
@@ -0,0 +1,28 @@
+package pl.java.scalatech;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport{
+    @Bean
+    public InternalResourceViewResolver InternalResourceViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/templates/");
+    viewResolver.setSuffix(".jsp");
+    return viewResolver;
+    }
+    
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("WEB-INF/static/favicon.ico");
+        registry.addResourceHandler("/js/**").addResourceLocations("WEB-INF/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/css/");
+    
+    }
+    
+}
