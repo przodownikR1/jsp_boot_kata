47344b235464ab41b2a19da6dde31e3d850ba9a4
Slawomir Borowiec <przodownik@tlen.pl>
2014-10-31 19:45:49 +0100

    clean code

--- src/main/java/pl/java/scalatech/config/DsConfig.java
+++ src/main/java/pl/java/scalatech/config/DsConfig.java
@@ -1,16 +1,19 @@
 package pl.java.scalatech.config;
 
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -25,40 +28,73 @@ public class DsConfig {
     private String dialect;
     @Value("${spring.jpa.show-sql}")
     private boolean sqlShowFlag;
-    @Value("${spring.jpa.generate-ddl}")
+    @Value("${spring.jpa.hibernate.ddl-auto}")
     private String ddlGenerate;
     @Value("${hibernate.show.sql}")
     private boolean formatSql;
     
-    @Bean
+  /*  @Bean
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }*/
+    
+    @Bean
+    @DependsOn(value="h2Server")
+    DataSource dataSource(Server h2Server) {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:h2:tcp://localhost:9092/mem:przodownik;DB_CLOSE_DELAY=-1");
+        return dataSource;
+    }
+    
+    
+    @Bean(name = "h2Server", initMethod = "start", destroyMethod = "stop")
+    @DependsOn(value = "h2WebServer")
+    public org.h2.tools.Server createTcpServer() throws SQLException {
+        return org.h2.tools.Server.createTcpServer("-tcp,-tcpAllowOthers,-tcpPort,9092".split(","));
     }
 
+    @Bean(name = "h2WebServer", initMethod = "start", destroyMethod = "stop")
+    public org.h2.tools.Server createWebServer() throws SQLException {
+        return org.h2.tools.Server.createWebServer("-web,-webAllowOthers,-webPort,8082".split(","));
+    }
+    
+    
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    @DependsOn(value="dataSource")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws SQLException {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setDataSource(dataSource(createTcpServer()));
         entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         entityManagerFactoryBean.setPackagesToScan("pl.java.scalatech.domain");
         entityManagerFactoryBean.setJpaProperties(jpaProperties());
         return entityManagerFactoryBean;
     }
 
+    
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+    
     private Properties jpaProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", ddlGenerate);
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", dialect);
         properties.setProperty("hibernate.show_sql", ""+sqlShowFlag);
         properties.setProperty("hibernate.format_sql", ""+formatSql);
         return properties;
     }
 
+   
+    
     @Bean
-    public JpaTransactionManager transactionManager() {
+    public JpaTransactionManager transactionManager() throws SQLException {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        transactionManager.setDataSource(dataSource());
+        transactionManager.setDataSource(dataSource(createTcpServer()));
         return transactionManager;
     }
 }
