05f4573906e23c876892412842bf41a97115b3ec
Slawomir Borowiec <przodownik@tlen.pl>
2014-11-16 11:40:03 +0100

    messages added

--- src/main/java/pl/java/scalatech/config/WebConfig.java
+++ src/main/java/pl/java/scalatech/config/WebConfig.java
@@ -1,12 +1,28 @@
 package pl.java.scalatech.config;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport{
+    
+    @Autowired
+    private Environment env;
+
+    
     @Bean
     public InternalResourceViewResolver InternalResourceViewResolver() {
     InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -25,5 +41,30 @@ public class WebConfig extends WebMvcConfigurationSupport{
         registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/css/");
     
     }
-    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean(name = AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.forLanguageTag(env.getProperty("language")));
+        return clr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
 }
